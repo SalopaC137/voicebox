7245b981bafdc4f174e6247f02f26c0bf117e2fd
SalopaC137 <kmcharles024@gmail.com>
2026-04-25 20:38:49 +0300

    error fix 4 .env

--- server/src/index.js
+++ server/src/index.js
@@ -71,7 +71,7 @@ if (!process.env.MONGO_URI) {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`🚀  Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`🚀  Server running on port ${PORT}`));
 
 async function connectToMongo() {
   try {
